be038f2c5143f93a489f6bd10ecf364b235ffaef
Lawrence Dunn <dunnla@seas.upenn.edu>
2022-11-05 21:39:23 -0400

    Add simple Ruby driver for the command line interface

---
+++ make.rb
@@ -0,0 +1,31 @@
+$out_dir = "_out"
+$nodes_dir = "_out/nodes"
+
+Dir.mkdir $out_dir unless File.directory?($out_dir)
+Dir.mkdir $nodes_dir unless File.directory?($nodes_dir)
+
+commands = ["logo", "cube", "tree"]
+large_sizes = ["512", "256", "128", "96"]
+fdout = File.open("#{$out_dir}/output.txt", "a")
+fderr = File.open("#{$out_dir}/error.txt", "a")
+commands.each do |cmd|
+  large_sizes.each do |size|
+    Kernel.spawn("cabal run tealeaves-art -- --selection #{cmd} --output _out/#{cmd}_#{size}.svg --width #{size}",
+                 :out => fdout, :err => fderr)
+  end
+end
+fdout.close
+fderr.close
+
+nodes = ["node-f", "node-d", "node-dt", "node-m", "node-dm", "node-dt", "node-dtm"]
+small_sizes = ["64", "32", "16"]
+fdout = File.open("#{$nodes_dir}/output.txt", "a")
+fderr = File.open("#{$nodes_dir}/error.txt", "a")
+nodes.each do |cmd|
+  small_sizes.each do |size|
+    Kernel.spawn("cabal run tealeaves-art -- --selection #{cmd} --output _out/nodes/#{cmd}_#{size}.svg --width #{size}",
+                 :out => fdout, :err => fderr)
+  end
+end
+fdout.close
+fderr.close
